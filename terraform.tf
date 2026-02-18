58a0806d33dee42bb89b1dcd254b917263c44361
Davi Leal <162625219+davileal0702@users.noreply.github.com>
2026-02-18 16:37:03 -0300

    Add files via upload

---
+++ terraform.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.92"
+    }
+  }
+
+  required_version = ">= 1.2"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name = "teste"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  key_name = "iac-alura"
+#  user_data = <<-EOF
+#                #!/bin/bash
+#                cd /home/ubu
+#                echo "<h1>Feito com Terraform</h1>" > index.html
+#                nohup busybox httpd -f -p 8080 &
+#                EOF
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
